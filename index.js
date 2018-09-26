ee5168c80e9383e9999248cedb2a6eef947639b9
Alfred Law <alfred.lawsn@gmail.com>
2018-09-26 21:06:01 +0000

    created function that adds from A to B recursively

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if (fromN === toN) {
+    return fromN;
+  }
+ 
+  return toN + sum(fromN, toN - 1)
 }
 
 module.exports = sum;
+
+// console.log(sum(1, 1));
+// console.log(sum(0, 0));
+// console.log(sum(5, 5));
+// console.log(sum(0, 1));
+// console.log(sum(1, 2));
+// console.log(sum(3, 7));
+
+
